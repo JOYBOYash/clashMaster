6799155aa573b1d7a9b1b606b9f4c1686ff0370a
Sri Swaroop Kumar Akili <joyboysskofficially@gmail.com>
2025-07-05 14:13:55 +0000

    first of all change the colors and use coc related color theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,19 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
+import { Inter, Lilita_One } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import { cn } from '@/lib/utils';
+
+const fontBody = Inter({
+  subsets: ['latin'],
+  variable: '--font-body',
+})
+
+const fontHeadline = Lilita_One({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-headline',
+})
 
 export const metadata: Metadata = {
   title: 'Clash Master',
@@ -14,12 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">
+      <body className={cn("font-body antialiased", fontBody.variable, fontHeadline.variable)}>
         {children}
         <Toaster />
       </body>

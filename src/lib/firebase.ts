46288c8727539e97a094e1c83a9e928afcd81762
Sri Swaroop Kumar Akili <joyboysskofficially@gmail.com>
2025-07-07 10:11:10 +0000

    no need to hardcode, use env instead

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAtmhUHkk5UZa_3SvY48ztwsPX6p685dKA",
-  authDomain: "clashmaster-9344e.firebaseapp.com",
-  projectId: "clashmaster-9344e",
-  storageBucket: "clashmaster-9344e.appspot.com",
-  messagingSenderId: "142180503144",
-  appId: "1:142180503144:web:2b0e75afca8451d05b75dd",
-  measurementId: "G-240XZY7RJT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase

233987904bc25c535cd95101ddc2941be3e66d2a
Sri Swaroop Kumar Akili <joyboysskofficially@gmail.com>
2025-07-07 10:08:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,15 +6,14 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAtmhUHkk5UZa_3SvY48ztwsPX6p685dKA",
+  authDomain: "clashmaster-9344e.firebaseapp.com",
+  projectId: "clashmaster-9344e",
+  storageBucket: "clashmaster-9344e.appspot.com",
+  messagingSenderId: "142180503144",
+  appId: "1:142180503144:web:2b0e75afca8451d05b75dd",
+  measurementId: "G-240XZY7RJT"
 };
 
 // Initialize Firebase

ab8f4b3dfd0e2dfb348912d8a60264f62e07d0f5
Sri Swaroop Kumar Akili <joyboysskofficially@gmail.com>
2025-07-13 16:58:42 +0000

    use both the fonts in the layout, for the app to recognsie the fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("antialiased")}>
+      <body className={cn("antialiased font-body font-headline")}>
         <AuthProvider>
           <AppWrapper>
             {children}

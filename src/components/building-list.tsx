db739770574c852626194b36cf824a33062a773e
Sri Swaroop Kumar Akili <joyboysskofficially@gmail.com>
2025-07-15 05:33:25 +0000

    bro don't use the image-paths file for those anymore, directly handle th

--- src/components/building-list.tsx
+++ src/components/building-list.tsx
@@ -9,7 +9,52 @@ import { Shield, Coins, Sword, SlidersHorizontal, Settings2, Hammer } from 'luci
 import { Progress } from '@/components/ui/progress';
 import Image from 'next/image';
 import { buildingNameToType } from '@/lib/constants';
-import { nameToPathMap, defaultImagePath } from '@/lib/image-paths';
+
+const defaultImagePath = 'https://placehold.co/128x128.png';
+const nameToPathMap: Record<string, string> = {
+    'Cannon': '/assets/_defenses/cannon/Cannon_1.png',
+    'Archer Tower': '/assets/_defenses/archer-tower/Archer_Tower_1.png',
+    'Mortar': '/assets/_defenses/mortar/Mortar_1.png',
+    'Air Defense': '/assets/_defenses/air-defense/Air_Defense_1.png',
+    'Wizard Tower': '/assets/_defenses/wizard-tower/Wizard_Tower_1.png',
+    'Air Sweeper': '/assets/_defenses/air-sweeper/Air_Sweeper_1.png',
+    'Hidden Tesla': '/assets/_defenses/hidden-tesla/Hidden_Tesla_1.png',
+    'Bomb Tower': '/assets/_defenses/bomb-tower/Bomb_Tower_1.png',
+    'X-Bow': '/assets/_defenses/x-bow/X-Bow_1.png',
+    'Inferno Tower': '/assets/_defenses/inferno-tower/Inferno_Tower_1.png',
+    'Eagle Artillery': '/assets/_defenses/eagle-artillery/Eagle_Artillery_1.png',
+    'Scattershot': '/assets/_defenses/scattershot/Scattershot_1.png',
+    'Spell Tower': '/assets/_defenses/spell-tower/Spell_Tower_1.png',
+    'Monolith': '/assets/_defenses/monolith/Monolith_1.png',
+    'Ricochet Cannon': '/assets/_defenses/ricochet-cannon/Ricochet_Cannon_1.png',
+    'Multi-Archer Tower': '/assets/_defenses/multi-archer-tower/Multi-Archer_Tower_1.png',
+    'Wall': '/assets/_buildings/walls/Wall_1.png',
+    'Gold Mine': '/assets/_resources/collectors/gold-mines/Gold_Mine_1.png',
+    'Elixir Collector': '/assets/_resources/collectors/elixir-collector/Elixir_Collector_1.png',
+    'Dark Elixir Drill': '/assets/_resources/collectors/dark-elixir-drills/Dark_Elixir_Drill_1.png',
+    'Gold Storage': '/assets/_resources/storages/gold-storage/Gold_Storage_1.png',
+    'Elixir Storage': '/assets/_resources/storages/elixir-storage/Elixir_Storage_1.png',
+    'Dark Elixir Storage': '/assets/_resources/storages/dark-elixir-storage/Dark_Elixir_Storage_1.png',
+    'Army Camp': '/assets/_buildings/army-camp/Army_Camp_1.png',
+    'Barracks': '/assets/_buildings/barracks/Barracks_1.png',
+    'Dark Barracks': '/assets/_buildings/dark-barracks/Dark_Barracks_1.png',
+    'Laboratory': '/assets/_buildings/laboratory/Laboratory_1.png',
+    'Spell Factory': '/assets/_buildings/spell-factory/Spell_Factory_1.png',
+    'Dark Spell Factory': '/assets/_buildings/dark-spell-factory/Dark_Spell_Factory_1.png',
+    'Workshop': '/assets/_buildings/workshop/Workshop_1.png',
+    'Pet House': '/assets/_buildings/pet-house/Pet_House_1.png',
+    'Blacksmith': '/assets/_buildings/blacksmith/Blacksmith_1.png',
+    'Town Hall': '/assets/_town-halls/Town_Hall_1.png',
+    'Clan Castle': '/assets/_buildings/clan-castle/Clan_Castle_1.png',
+    'Builder Hut': '/assets/_buildings/builders-hut/Builder_s_Hut_1.png',
+    'Bomb': '/assets/_traps/bomb/Bomb_1.png',
+    'Spring Trap': '/assets/_traps/spring-trap/Spring_Trap_1.png',
+    'Air Bomb': '/assets/_traps/air-bomb/Air_Bomb_1.png',
+    'Giant Bomb': '/assets/_traps/giant-bomb/Giant_Bomb_1.png',
+    'Seeking Air Mine': '/assets/_traps/seeking-air-mine/Seeking_Air_Mine_1.png',
+    'Skeleton Trap': '/assets/_traps/skeleton-trap/Skeleton_Trap_1.png',
+    'Tornado Trap': '/assets/_traps/tornado-trap/Tornado_Trap_1.png',
+};
 
 const getGroupedBuildings = (buildings: Building[]) => {
     const buildingMap: { [key: string]: { building: Building; count: number } } = {};
